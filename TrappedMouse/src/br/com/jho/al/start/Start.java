4313d523d223f201ce290d12a54b2206e319b082
Alana Cristina <acsffreitas@hotmail.com>
2016-05-26 14:49:49 -0300

    Implementação classe main

--- TrappedMouse/src/br/com/jho/al/start/Start.java
+++ TrappedMouse/src/br/com/jho/al/start/Start.java
@@ -1,10 +1,30 @@
 package br.com.jho.al.start;
 
-public class Start {
+import java.util.Scanner;
+
+import br.com.jho.al.stackmanager.MyStack;
+
+public class Start<T> {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int size;
+		String row = "";
+		
+		Scanner in = new Scanner(System.in);
+		MyStack stack;
+		
+		System.out.println("Digite o número de linhas para o labirinto: ");
+		size = in.nextInt();
+		
+		stack = new MyStack(size);
+		
+		for (int i = 0; i < size; i++) {
+			System.out.println("Linha: ");
+			row = in.next();
+			stack.push(row);
+		}
+		
+		stack.print();
 	}
-
 }
