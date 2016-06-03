adcdb33ca52d0a243bc332504e3dbb8f408343e2
Alana Cristina <acsffreitas@hotmail.com>
2016-06-03 17:23:27 -0300

    Classe para animação

--- TrappedMouse/src/br/com/jho/al/swing/GraphicMaze.java
+++ TrappedMouse/src/br/com/jho/al/swing/GraphicMaze.java
@@ -12,21 +12,44 @@ import javax.swing.*;
  *
  * @author alana
  */
-public class GraphicMaze {
+public class GraphicMaze extends JPanel{
     
     Maze maze = new Maze();
-    JFrame frame = new JFrame();
-    JPanel panel = new JPanel();
+    JFrame frame;
+    JPanel panel;
+    
+    String m = "";
     
     public void showMaze() {
         
+        startWindow();
+        startPanelMain();
+        showWindow();
+    }
+    
+    public void print() {
+        for (int i = 0; i < maze.getSizeRow(); i++) {
+            for (int j = 0; j < maze.getSizeCol(); j++) {
+                m = String.valueOf(maze.getMaze());
+                panel.add(m, this);
+            }
+        }
+    }
+   
+    public void startWindow() {
+        frame = new JFrame("Maze");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public void startPanelMain() {
+        panel = new JPanel();
+        print();
         frame.add(panel);
+    }
+    
+    public void showWindow() {
+        //frame.pack();
+        frame.setSize(600, 400);
         frame.setVisible(true);
     }
-    //panel.add(maze);
-    
-    //maze.
-    
-    //JOptionPane.showMessageDialog (parentComponent, maze);
-    //JOptionPane.showMessageDialog();
 }
